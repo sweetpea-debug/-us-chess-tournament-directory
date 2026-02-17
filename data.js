40fb8b387871cc6da109980502d4305589eb6772
sweetpea-debug <darius.zolnor@gmail.com>
2026-02-17 09:46:26 -0500

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,16 @@
+// data.js
+export const SOURCE_CATALOG = [
+  {
+    id: "uschess-upcoming",
+    name: "US Chess (new.uschess.org)",
+    homepage: "https://new.uschess.org/upcoming-tournaments",
+  },
+  {
+    id: "michess",
+    name: "Michigan Chess Association",
+    homepage: "https://www.michess.org/events",
+  },
+];
+
+// IMPORTANT: fallback should be empty and should NOT be cached as “good data”.
 export const FALLBACK_EVENTS = [];
